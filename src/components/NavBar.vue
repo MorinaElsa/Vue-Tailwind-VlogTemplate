9f650d30888a5e33423305e9c5d43558da4c7e00
MorinaElsa <morinaelsa01@gmail.com>
2024-04-29 11:52:27 +0200

    fixing sidebar icon

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,31 +1,49 @@
 <template>
   <nav class="fixed top-0 z-50 w-full bg-white shadow-lg">
-    <div class="flex justify-between items-center h-16 px-4 max-w-[1240px] mx-auto">
+    <div
+      class="flex justify-between items-center h-16 px-4 max-w-[1240px] mx-auto"
+    >
       <h2 class="text-3xl font-cabin hover:text-hover-green">Meranda</h2>
       <div class="sm:hidden">
-        <button @click="toggleMenu" class="text-xl text-gray-800 focus:outline-none">
-          ☰
+        <button
+          @click="toggleMenu"
+          class="text-xl text-gray-800 focus:outline-none"
+        >
           <template v-if="isMenuOpen">
-            <path d="M6 18L18 6M6 6l12 12"></path>
+            <div>X</div>
           </template>
           <template v-else>
-            <path d="M4 6h16M4 12h16m-7 6h7"></path>
+            <div>☰</div>
           </template>
         </button>
       </div>
-      <ul :class="`uppercase sm:flex ${isMenuOpen ? 'block' : 'hidden'} font-mono sm:mt-0 mt-4 absolute sm:relative top-full left-0 sm:top-0 sm:left-auto sm:flex flex-col sm:flex-row bg-white sm:bg-transparent w-full sm:w-auto justify-center items-center`">
-        <li class="p-4 hover:text-hover-green" @click="closeMenu"><a href="/"> Home </a></li>
-        <li class="p-4 hover:text-hover-green" @click="closeMenu"><a href="#trending"> Trending </a></li>
-        <li class="p-4 hover:text-hover-green" @click="closeMenu"><a href="#influencers"> Influencers </a></li>
-        <li class="p-4 hover:text-hover-green" @click="closeMenu"><a href="#health"> Health </a></li>
-        <li class="p-4 hover:text-hover-green" @click="closeMenu"><a href="#subscribe"> Subscribe </a></li>
+      <ul
+        :class="`uppercase sm:flex ${
+          isMenuOpen ? 'block' : 'hidden'
+        } font-mono mt-4 absolute sm:relative top-full left-0 sm:left-auto sm:flex flex-col sm:flex-row bg-white sm:bg-transparent w-full sm:w-auto justify-center items-center`"
+      >
+        <li class="p-4 hover:text-hover-green" @click="closeMenu">
+          <a href="/"> Home </a>
+        </li>
+        <li class="p-4 hover:text-hover-green" @click="closeMenu">
+          <a href="#trending"> Trending </a>
+        </li>
+        <li class="p-4 hover:text-hover-green" @click="closeMenu">
+          <a href="#influencers"> Influencers </a>
+        </li>
+        <li class="p-4 hover:text-hover-green" @click="closeMenu">
+          <a href="#health"> Health </a>
+        </li>
+        <li class="p-4 hover:text-hover-green" @click="closeMenu">
+          <a href="#subscribe"> Subscribe </a>
+        </li>
       </ul>
     </div>
   </nav>
 </template>
 
 <script>
-import { ref } from 'vue';
+import { ref } from "vue";
 
 export default {
   setup() {
@@ -42,9 +60,8 @@ export default {
     return {
       isMenuOpen,
       toggleMenu,
-      closeMenu
+      closeMenu,
     };
-  }
+  },
 };
 </script>
-
