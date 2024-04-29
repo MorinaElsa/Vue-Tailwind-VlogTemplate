86389c443f335546a9079f1f35c8ee3457f2d2a1
MorinaElsa <morinaelsa01@gmail.com>
2024-04-29 10:08:53 +0200

    changing EditorsPrick file to EditorPick

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,14 @@ import HomeVue from "./components/Home.vue";
 import NavBar from "./components/NavBar.vue";
 import SubscribeVue from "./components/Subscribe.vue";
 import HealthVue from "./components/Health.vue";
-import EditorsPickVue from "./components/EditorsPick.vue";
 import InfluencersVue from "./components/Influencers.vue";
+import EditorPickVue from "./components/EditorPick.vue";
 </script>
 
 <template>
   <NavBar />
   <HomeVue />
-  <EditorsPickVue />
+  <EditorPickVue />
   <InfluencersVue />
   <HealthVue />
   <SubscribeVue />
