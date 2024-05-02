e5d13af5de9191ea26a6ed9c4d2bc2fb9adf0766
MorinaElsa <morinaelsa01@gmail.com>
2024-05-02 13:47:45 +0200

    data change

--- src/assets/data/influencersData.js
+++ src/assets/data/influencersData.js
@@ -1,38 +1,37 @@
 import imgInfluencer from "../img/influencer.png";
 const dataInfluencers = [
-    {
-      Id: "1",
-      img: imgInfluencer,
-      quote:
-        " Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam. ",
-      fullname: " John Doe ",
-      place: "Kosovo",
-    },
-    {
-      Id: "2",
-      img: imgInfluencer,
-      quote:
-        " Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.  ",
-      fullname: " John Doe ",
-      place: "Moroko",
-    },
-    {
-      Id: "3",
-      img:imgInfluencer,
-      quote:
-        " Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
-      fullname: " John Doe ",
-      place: "Albania",
-    },
-    {
-      Id: "4",
-      img: imgInfluencer,
-      quote:
-        " Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam. ",
-      fullname: " John Doe ",
-      place: "Malta",
-    },
-  ];
-  
-  export default dataInfluencers;
-  
+  {
+    Id: "1",
+    img: imgInfluencer,
+    quote:
+      " Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam. ",
+    fullname: " Louise Kelly ",
+    place: "Kosovo",
+  },
+  {
+    Id: "2",
+    img: imgInfluencer,
+    quote:
+      " Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.  ",
+    fullname: " Louise Kelly ",
+    place: "Kosovo",
+  },
+  {
+    Id: "3",
+    img: imgInfluencer,
+    quote:
+      " Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.",
+    fullname: " Louise Kelly ",
+    place: "Kosovo",
+  },
+  {
+    Id: "4",
+    img: imgInfluencer,
+    quote:
+      " Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam. ",
+    fullname: " Louise Kelly ",
+    place: "Kosovo",
+  },
+];
+
+export default dataInfluencers;
